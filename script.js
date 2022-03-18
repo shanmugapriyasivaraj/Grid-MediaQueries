b30f7c93de96522ebb3cb94e7b982a2f34e607e1
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-03-18 20:53:50 +0530

    Map&Filter

--- script.js
+++ script.js
@@ -7,16 +7,23 @@
 
 // Promise
 
-const drive = new Promise((resolve, reject) => {
-  let age = 19;
-  if (age >= 18) {
-    resolve("Eligible");
-  } else {
-    reject("Not Eligible");
-  }
-});
+// const drive = new Promise((resolve, reject) => {
+//   let age = 19;
+//   if (age >= 18) {
+//     resolve("Eligible");
+//   } else {
+//     reject("Not Eligible");
+//   }
+// });
 
-drive
-  .then((msg) => "Awesome you are " + msg)
-  .then((newMsg) => console.log(newMsg))
-  .catch((errMsg) => console.log(errMsg));
+// drive
+//   .then((msg) => "Awesome you are " + msg)
+//   .then((newMsg) => console.log(newMsg))
+//   .catch((errMsg) => console.log(errMsg));
+
+console.log(
+  [1, 2, 3, 4]
+    .map((x) => x + 1)
+    .filter((x) => x > 3)
+    .map((y) => y * 2)
+);
