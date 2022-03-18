8151c250f8863381bea55c530e7c0b77abc74e32
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-03-18 16:38:47 +0530

    Promise

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-setTimeout(function timer1() {
-  console.log("Hii..");
-  setTimeout(function timer2() {
-    console.log("Awesome");
-  }, 3000);
-}, 2000);
+// setTimeout(function timer1() {
+//   console.log("Hii..");
+//   setTimeout(function timer2() {
+//     console.log("Awesome");
+//   }, 3000);
+// }, 2000);
+
+// Promise
+
+const drive = new Promise((resolve, reject) => {
+  let age = 19;
+  if (age >= 18) {
+    resolve("Eligible");
+  } else {
+    reject("Not Eligible");
+  }
+});
+
+drive
+  .then((msg) => "Awesome you are " + msg)
+  .then((newMsg) => console.log(newMsg))
+  .catch((errMsg) => console.log(errMsg));
